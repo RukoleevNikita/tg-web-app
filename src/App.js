99112c607f2eaa21cdd3d00227b39344b49a3c90
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-04 19:33:39 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from '../src/hooks/useTelegram';
+import { Header } from './components/Header/Header';
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -13,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      test bot
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
