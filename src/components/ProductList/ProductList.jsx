1cad7200bceeed2c8581ccf3a13fa3a0136a849f
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-05 20:12:47 +0600

    test

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import ProductItem from '../ProductItem/ProductItem';
 import './ProductList.css';
 
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+];
+
+const getTotalPrice = (items = []) => { // суммирование цены
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+};
+
 export const ProductList = () => {
+    const [addedItems, setAddedItems] = React.useState([]); // корзина
+    const {tg, queryId} = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id); // найти товар в корзине
+        let newItems = [];
+
+        if(alreadyAdded) { // если товар есть удалить
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product]; // если его нет то в конец корзины добавляем этот товар
+        }
+
+        setAddedItems(newItems); // сохраняем состояние
+
+        if(newItems.length === 0) { // если товары из корзины удалены то кнопку скрыть
+            tg.MainButton.hide();
+        } else { // иначе показать и подсчитать цену
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
-        <div>
-            ProductList
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
         </div>
     )
 }
