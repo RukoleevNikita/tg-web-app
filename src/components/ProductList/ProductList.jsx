c60486121930473826217bcbc71c31ddd1161864
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-05 19:19:36 +0600

    test

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductList.css';
+
+export const ProductList = () => {
+    return (
+        <div>
+            ProductList
+        </div>
+    )
+}
+
+export default ProductList;
