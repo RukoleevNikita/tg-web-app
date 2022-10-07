1abeabf1a65eaa19abd30142662d9de09d52befa
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-07 14:40:33 +0600

    test

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,10 +5,10 @@ import React, {useState} from 'react';
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 222, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 3333, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 111, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 1111, description: 'Зеленого цвета, теплая'},
     {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
     {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
     {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
