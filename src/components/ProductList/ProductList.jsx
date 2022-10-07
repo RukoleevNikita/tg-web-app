021941bf17d3699fb071aa1bd8e11f398c2b2fbc
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-07 17:57:56 +0600

    test

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,14 +6,14 @@ import {useCallback, useEffect} from "react";
 import { useRef } from 'react';
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 222, description: 'Синего цвета, прямые', url: '../src/assets/products/1_1.jpg'},
-    {id: '2', title: 'Куртка', price: 3333, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 111, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 1111, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 222, description: 'Синего цвета, прямые', url: 'http://localhost:8000/assets/products/1_1.jpg'},
+    {id: '2', title: 'Куртка', price: 3333, description: 'Зеленого цвета, теплая', url: 'http://localhost:8000/assets/products/3_1.jpg'},
+    {id: '3', title: 'Джинсы 2', price: 111, description: 'Синего цвета, прямые', url: 'http://localhost:8000/assets/products/4_1.jpg'},
+    {id: '4', title: 'Куртка 8', price: 1111, description: 'Зеленого цвета, теплая', url: 'http://localhost:8000/assets/products/5_1.jpg'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые', url: 'http://localhost:8000/assets/products/6_1.jpg'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая', url: 'http://localhost:8000/assets/products/7_1.jpg'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые', url: 'http://localhost:8000/assets/products/8_1.jpg'},
+    // {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', url: 'http://localhost:8000/assets/products/9_1.jpg'},
 ];
 
 const getTotalPrice = (items = []) => { // суммирование цены
