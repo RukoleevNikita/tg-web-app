846cecee98ddbca2659fbf857a4af73b63fbac37
Никита Руколеев <nikitarukoleev@MacBook-Pro-Nikita.local>
2022-10-15 19:30:20 +0600

    test

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -13,7 +13,7 @@ const ProductItem = ({product, className, onAdd}) => {
         <div className={'product ' + className}>
             {/* <div className={'img'}/> */}
             <img src={product.url} alt="" className={'img'} />
-            <div className={'title'}>{product.title}</div>
+            <a className={'title'}>{product.title}</a>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
